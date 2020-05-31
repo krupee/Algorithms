c1c6ac6a9d998e87b82da1f4d89acc22c0873f71
krupee <krupal.patel21@gmail.com>
2020-05-31 16:04:38 -0400

    Queue 1

---
+++ Queue.py
@@ -0,0 +1,32 @@
+class Queue():
+    def __init__(self, data = []):
+        self.data = []
+
+    def pop(self):
+        return self.data.pop(0)
+
+    def push(self, new_value):
+        self.data.append(new_value)
+    
+    def peek(self):
+        print("Peek: ", self.data[0])
+
+
+def main():
+    q = Queue()
+
+    print(q.data)
+
+    q.push(1)
+    q.push(3)
+    q.push(5)
+
+    print(q.data)
+
+    q.pop()
+    q.pop()
+
+    print(q.data)
+
+main()
+
